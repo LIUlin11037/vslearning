c46ed7ccd0a4aa56d5c9b0db9a7d4c58aa9f1e47
LIUlin11037 <201631575@qq.com>
2025-10-07 10:42:13 +0800

    break,continue
    train

--- Project14/FileName.cpp
+++ Project14/FileName.cpp
@@ -1,1 +1,97 @@
-zheshi
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+////平均成绩计算break
+//{
+//	int i = 0;
+//	float sum = 0;
+//	float score = 0;
+//	for (i = 1; i <= 6; i++)
+//	{
+//		scanf("%f", &score);
+//		sum = sum + score;
+//		if (score < 0)
+//			break;
+//	}
+//	int n = i - 1;
+//	float average;
+//	average = sum/n;
+//	printf("the  score is:%f", average);
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= 5; i++)
+//	{
+//		if (i == 3)
+//			continue;
+//		printf("%d", i);
+//	return 0;
+//}
+int main()
+{
+	int i = 1;
+	int j = 0;
+	for (i = 1; i <= 5; i++)
+	{for
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
